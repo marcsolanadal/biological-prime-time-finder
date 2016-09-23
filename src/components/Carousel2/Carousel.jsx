c2a1ee71e70e6293ae317ddd5b86c5e85f98b3df
Marc Sola Nadal <ionthasmail@gmail.com>
2016-09-23 17:46:26 +0200

    Swipes working with the classnames assignation logic optimized but still with the drag bug

--- src/components/Carousel2/Carousel.jsx
+++ src/components/Carousel2/Carousel.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-// import update from 'react-addons-update'
-import cx from 'classnames'
+
+import classNames from 'classnames'
 
 import styles from './Carousel.css'
 
@@ -37,15 +37,11 @@ class Carousel extends React.Component {
   swipeMove (e) {
     if (this.state.isSwiping) { // Detecting the swipe
       if (this.state.optimizationCounter > 3) { // Optimizing the sample rate of the swipe
-        const direction = (this.state.swipePositionEnd - e.pageX < 0) ? 5 : -5
-        const dx = this.state.swipePositionEnd - this.state.swipePositionStart
-        //console.log(`start: ${this.state.swipePositionStart}, end: ${this.state.swipePositionEnd}, dx: ${dx}`)
-        //console.log('state: ', this.state)
         this.setState({
           isTransitioning: (Math.abs(this.state.swipeCounter) > this.state.thereshold),
           swipePositionEnd: e.pageX,
-          swipeDirection: direction,
-          swipeCounter: dx,
+          swipeDirection: (this.state.swipePositionEnd - e.pageX < 0) ? 1 : -1,
+          swipeCounter: this.state.swipePositionEnd - this.state.swipePositionStart,
           optimizationCounter: 0
         })
       } else {
@@ -56,7 +52,6 @@ class Carousel extends React.Component {
   swipeEnd (e) {
     this.setState({
       isSwiping: false,
-      isTransitioning: false,
       swipeCounter: 0,
       swipePositionStart: 0,
       swipePositionEnd: 0
@@ -68,29 +63,45 @@ class Carousel extends React.Component {
     const counter = this.state.swipeCounter
     const isSwiping = this.state.isSwiping
     const isTransitioning = this.state.isTransitioning
-    const direction = this.state.swipeDirection
+
+    const left = (this.state.swipeDirection > 0)
+    const right = !left
 
     const slidePosition = (!isTransitioning)
       ? { transform: `translateX(${counter}px)` }
       : {}
 
-    let leftSlideStyle = cx(styles.leftSlide, {
-      [styles.leftSlideOut]: !isSwiping && !isTransitioning && (direction > 0),
-      [styles.leftSlideIn]: isTransitioning && (direction > 0)
-    })
+    let leftClassNames = {
+      [styles.leftSlideOut]: left && !isSwiping && !isTransitioning,
+      [styles.leftSlideIn]: left && isTransitioning
+    }
 
-    let rightSlideStyle = {}
+    let rightClassNames = {
+      [styles.rightSlideOut]: right && !isSwiping && !isTransitioning,
+      [styles.rightSlideIn]: right && isTransitioning
+    }
+
+    let leftSlideStyle = classNames(styles.leftSlide, leftClassNames)
+    let rightSlideStyle = classNames(styles.rightSlide, rightClassNames)
+    let centerSlideStyle = classNames(styles.centerSlide, leftClassNames, rightClassNames)
 
     return (
-      <div className={styles.overlay}
+      <div
         onMouseDown={this.swipeStart}
         onMouseMove={this.swipeMove}
-        onMouseUp={this.swipeEnd}
-        onMouseLeave={this.swipeEnd}>
+        onMouseUp={this.swipeEnd}>
 
-        <div style={slidePosition} className={leftSlideStyle} />
-        <div style={slidePosition} className={styles.centerSlide} />
-        <div className={rightSlideStyle} />
+        <div style={slidePosition} className={leftSlideStyle}>
+          <h1>Mercy</h1>
+        </div>
+
+        <div style={slidePosition} className={centerSlideStyle}>
+          <h1>Lucio</h1>
+        </div>
+
+        <div style={slidePosition} className={rightSlideStyle}>
+          <h1>Zenyatta</h1>
+        </div>
 
       </div>
     )
